ebe22af727bd559c761e0457624a98d38647c8c0
Kyle Ronning <kyleronning+kronning6@gmail.com>
2023-06-06 19:23:00 -0500

    Add scrapped images and names from Kipsu team page

---
+++ src/app/data.ts
@@ -0,0 +1,332 @@
+const d = {
+  "data": [
+    {
+      "name": "Chris Smith",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60d10a9fd6204b8a4b98eed0_Chris%20Smith%20V1.jpg"
+    },
+    {
+      "name": "Tom McGurran",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a007975c4357b43637d9_Tom-McGurran-EHS.jpg"
+    },
+    {
+      "name": "Brian Jacobson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249f6a73b38a04fc63d3ba_Brian-Jacobson-EHS.jpg"
+    },
+    {
+      "name": "Charlie Lehmann",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60d10c5179918a58d0edcf64_Charlie%20Lehman.jpg"
+    },
+    {
+      "name": "Drew Anunti",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60d10bad142456df0171aac2_Drew%20White%20brick%20v2.jpg"
+    },
+    {
+      "name": "Eva Dixon",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60d10b353f9ff6e83794e90a_EvaHeadshot.jpeg"
+    },
+    {
+      "name": "Justin Porter",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6318df4cc8fb340895b6a267_Justin-Porter-HS.jpg"
+    },
+    {
+      "name": "Othman Smihi",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249fb473b38a80e063f87d_Othman-Smihi-EHS.jpg"
+    },
+    {
+      "name": "Valerie Just",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6331e9c4fbc059afce1d592e_Val-Just-EHS.jpg"
+    },
+    {
+      "name": "Carolyn Cooper",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c9840c878df67b4602c464_Carolyn-Cooper-Website.png"
+    },
+    {
+      "name": "Hunter Mickelson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c98617dda8bd6fd5ed22f3_Hunter-Mickleson-Website1.png"
+    },
+    {
+      "name": "Jacob Schmidt",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60db890f999cf1011b74cf9c_Jacob-Schmidt-website.png"
+    },
+    {
+      "name": "Jenae Alderson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60db88df16d50291f199f9c6_Jenae-Alderson-website.png"
+    },
+    {
+      "name": "Kate Prostrollo",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/624215c39654fd599c020d7a_KateHeadshot_Website.png"
+    },
+    {
+      "name": "Marcus Sheeler",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/624214b3df8a33a096ca6fa9_MarcusHeadshot_website.png"
+    },
+    {
+      "name": "Megan Gillett",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a92524a7853f63c9c0a9_Megan-Gilllett-EHS.jpg"
+    },
+    {
+      "name": "Paul Witte",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c989d6ecaa4f85881b816d_Paul-Witte-Website%20(1).png"
+    },
+    {
+      "name": "Rashad West",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6242189fc0ee90706167ffb0_RashadHeadshot_website.png"
+    },
+    {
+      "name": "Sierra Thomsen",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60db8930218b20a8bcbb893d_Sierra-Thomsen-website.png"
+    },
+    {
+      "name": "TJ Aubrecht",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/618d816c39cc159b119c288e_TJ-Aubrecht-Website.png"
+    },
+    {
+      "name": "Zach Bordeau",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324ab177e76aa11b815a768_Zach-Bordeau-EHS.jpg"
+    },
+    {
+      "name": "Adam Gorski",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a5bd0e557510bcaf4580_Adam-Gorski-EHS.jpg"
+    },
+    {
+      "name": "Amber Sorenson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a68af9cb28720a7ec228_Amber-EHS-.jpg"
+    },
+    {
+      "name": "Camilo Welch",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249c6aebc7c061cf40f3fb_Camilo-Welch-EHS.jpg"
+    },
+    {
+      "name": "Emily Cox",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c98bb9f8fb483dea8427e5_Emily-Cox-Website.png"
+    },
+    {
+      "name": "Garrett Krohn",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a7864a356b1a90d1bf50_Garrett-Krohn-EHS.jpg"
+    },
+    {
+      "name": "Gretchen Nussbaum",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a7ca88e1cbcf764a8699_Gretchen-Nussbaum-EHS.jpg"
+    },
+    {
+      "name": "Heather Kim",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a80f54e04c986ad7e666_Heather-Kim-EHS.jpg"
+    },
+    {
+      "name": "Hue Vang",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/638782412ead53d5ce42531e_Hue-Vang-EHS.png"
+    },
+    {
+      "name": "Iloriem McLaughin",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a84b5094580614de14c6_Iloriam-McLaughin-EHS.jpg"
+    },
+    {
+      "name": "Jake Frisch",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249d82a1de489362eb702f_Jake-Frisch-EHS.jpg"
+    },
+    {
+      "name": "Jamie Dreyling",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a8854a562ff37e4d647e_Jamie-Dreyling-EHS.jpg"
+    },
+    {
+      "name": "Jonathan Alter",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/618d81c88179f262b8f39e9b_Jonathan-Alter-Website.png"
+    },
+    {
+      "name": "Jonny Briones",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/615b3a06811af55f2e8b9334_Jonny%20Briones%20Headshot%20Final.jpg"
+    },
+    {
+      "name": "Katie Shilkrot",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324bbbbacbd6f466264443f_Katie-Skilhort-EHS.jpg"
+    },
+    {
+      "name": "Kayci Pierce",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249c3e1ace14150bae4ff3_Kayci-Pierce-EHS.jpg"
+    },
+    {
+      "name": "Nick Kroeger",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a0bf4a356b103ace0cfc_Nick-Kroeger-EHS.jpg"
+    },
+    {
+      "name": "Nick Mathewson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6331ea8a1081b2ff29602b37_Nick-Mathewson-EHS.jpg"
+    },
+    {
+      "name": "Noah Theis",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a98b50945845afdfb988_Noah-Theis-EHS.jpg"
+    },
+    {
+      "name": "Todd Shneider",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c98f6d1ea7769e0af3f494_Todd-Shneider-Website.png"
+    },
+    {
+      "name": "Tony Trousdale",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60db88ff16d50221e399fc24_Tony-Trousdale-website.png"
+    },
+    {
+      "name": "Vilma Quito",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324aad124a785b430caeca6_Vilma-Quito-EHS.jpg"
+    },
+    {
+      "name": "Adam Assefa",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a64c4a562f12c44c4ef6_Adam-Assefa-EHS2.jpg"
+    },
+    {
+      "name": "Andrew Geier",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a50e4b4d50f04170e5d2_Andrew-Geier-EHS.jpg"
+    },
+    {
+      "name": "Austin Reese",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6331e994668e76872e7c3a0e_Austin-Reese-EHS.jpg"
+    },
+    {
+      "name": "Brian Merz",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a42d5a3d45509539dae7_Brian-Merz-EHS.jpg"
+    },
+    {
+      "name": "Cara Christiansen",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a3b200d7ce3fceec3998_Cara-Christensen-EHS.jpg"
+    },
+    {
+      "name": "Chee Lee",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a4ee6185e363983fff99_Chee-Lee-EHS.jpg"
+    },
+    {
+      "name": "Det Sorthepharack",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60db8985700de4fe0d95f388_Detsivith-Sorthepharack-website.png"
+    },
+    {
+      "name": "Doug Schmitz",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a21c4b4d503fa46e6041_Doug-Schmitz-EHS.jpg"
+    },
+    {
+      "name": "Eric Kleppen",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63878274e1189286b973058e_Eric-Kleppen-EHS.png"
+    },
+    {
+      "name": "Evan Lundell",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/639cc1346335d3a3717624e4_Evan-Lundell-EHS.png"
+    },
+    {
+      "name": "Gavin Fusco",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/632b2c6c31b5e40ec841e885_Gavin-Fusco-EHS.jpg"
+    },
+    {
+      "name": "John Cloeter",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6321eb0f893929bcfe7194a2_John-Cloeter-EHS.jpg"
+    },
+    {
+      "name": "Jon Harder",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c980ac0ccaf204f03c656a_Jon%20Harder.jpg"
+    },
+    {
+      "name": "Jordan Carr",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a8c3c5d4621589c06fca_Jordan-Carr-EHS.jpg"
+    },
+    {
+      "name": "Keelie Swafford",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c98fd80ccaf2a2533c9953_Keelie-Swafford-Website.png"
+    },
+    {
+      "name": "Kyle Ronning",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/642727be2b8d92d464d5d681_kyle-r-bigger-800-500-website.jpg"
+    },
+    {
+      "name": "Leo Alfred",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6346eb279b7d7d7f1e333283_Leo-Alfred-EHS.png"
+    },
+    {
+      "name": "Lukas Hirsch",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/62431e240f3b03ae234c33b5_Lukas-Hirsch-Website.png"
+    },
+    {
+      "name": "Matt Bouc",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/618d83386016044c83876cae_Matthew-Bouc-Website.png"
+    },
+    {
+      "name": "Minh Nguyen",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/62431e1d2a8b4ca218793c38_Minh-Nguyen-Website.png"
+    },
+    {
+      "name": "Nuru-Ghana (Nuru)  Ba'th",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6346eae503803a4a0b61caba_Nuru-Ba%27th-EHS.png"
+    },
+    {
+      "name": "Peter Duryea",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324aa3676352e0c1ccc1b7a_Peter-Duryea-EHS.jpg"
+    },
+    {
+      "name": "Peter Soung",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/634719184638f1b76d0d01d3_Peter-Soung-EHS.png"
+    },
+    {
+      "name": "Ryan Bloom",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a354f808e5c78faf5089_Ryan-Bloom-EHS.jpg"
+    },
+    {
+      "name": "Sean Olson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c98ea9f3b9a29f41341869_Sean-Olson-Website.png"
+    },
+    {
+      "name": "Smitha Sudheendra",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324aa7988e1cb80694ae5ed_Smitha-Sudheendra-EHS.jpg"
+    },
+    {
+      "name": "Tony Anderson",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249e5e52925b79fc5d8a71_Tony-Anderson-EHS.jpg"
+    },
+    {
+      "name": "Ashley Boegel",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/634dbfe7f9c266eec91f0397_Ashley-Boegel-EHS.png"
+    },
+    {
+      "name": "Cathryn Mulso",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249c17ebc7c01df340967f_Cathryn-Mulso-EHS.jpg"
+    },
+    {
+      "name": "Chanda Cheung",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60de1a89b6cd5025bf530dbe_Chanda-Cheung-website.png"
+    },
+    {
+      "name": "Danny Lindsey",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a58f798f314fd6609787_Danny-Lindsey-EHS.jpg"
+    },
+    {
+      "name": "Doug Stewart",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60de1b520f5041f19239bc14_Doug-Stewart-website.png"
+    },
+    {
+      "name": "Ethan Salau",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324a74cedc6e678dfd19a90_Ethan-Salau-EHS.jpg"
+    },
+    {
+      "name": "Joe Grunow",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/6324bd293d0076831bd3c3b8_Joe-Gunrow-EHS.jpg"
+    },
+    {
+      "name": "Abby Bartlett",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/633d880cd583ca9449f69672_Abby-Bartlett-EHS.jpg"
+    },
+    {
+      "name": "Bridget Fortin",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c990adb63698b3da009ec2_Bridget-Fortin-Website.png"
+    },
+    {
+      "name": "Matt Rode",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/60c99179b96b520e7c1a866f_Matt%20Rode%20Website.png"
+    },
+    {
+      "name": "Michael Davis",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/619d63571810a732ac1229d9_Michael-Davis-Website.png"
+    },
+    {
+      "name": "Aazib Tassadaq",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/63249dbf328d2854ec38ead4_Aazib-Tassadaq-EHS.jpg"
+    },
+    {
+      "name": "Will Cooper",
+      "img": "https://assets-global.website-files.com/60c0d5d6aa151a3b2c87c30f/618d81dcd94a71a34ecae249_Will-Cooper-Website.png"
+    }
+  ]
+};
